251e4233e71ddad40f6e1a081097b6856409dd25
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-08-27 16:07:58 -0300

    feat: prova p2

---
+++ POO/conta.py
@@ -0,0 +1,60 @@
+# Programação Orientada a Objetos
+# AC02 POO-EaD - Implementação de classes
+#
+# Email Impacta: ricardo.csantos@aluno.faculdadeimpacta.com.br
+
+class Conta:
+
+    def __init__(self, titular, agencia, numero, saldo_inicial):
+
+        self.__titular = titular
+        self.__agencia = agencia
+        self.__numero = numero
+        self.__saldo = saldo_inicial
+        self.__ativa = False
+        self.__operacoes = [('saldo inicial', saldo_inicial)]
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def agencia(self):
+        return self.__agencia
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def ativa(self):
+        return self.__ativa
+
+    @ativa.setter
+    def ativa(self, situacao):
+        if isinstance(situacao, bool):
+            self.__ativa = situacao
+
+    def depositar(self, valor):
+        if self.__ativa and valor > 0:
+            self.__saldo += valor
+            self.__operacoes.append(('deposito', valor))
+
+    def sacar(self, valor):
+        if self.__ativa and valor > 0 and self.__saldo >= valor:
+            self.__saldo -= valor
+            self.__operacoes.append(('saque', valor))
+
+    def transferir(self, conta_destino, valor):
+        contas_ativas = self.ativa and conta_destino.ativa
+        if contas_ativas and valor > 0 and self.saldo >= valor:
+            self.sacar(valor)
+            conta_destino.depositar(valor)
+            self.__operacoes.append(('transferencia', valor))
+
+    def tirar_extrato(self):
+        return self.__operacoes
